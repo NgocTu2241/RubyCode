88a7fc3ed9b6babb74cb9b5d43f2213e41e93424
Nguyen Tu <80658387+NgocTu2241@users.noreply.github.com>
2022-09-09 08:38:02 +0700

    Add files via upload

---
+++ NN Ruby/Bai1.rb
@@ -0,0 +1,18 @@
+print "Nhap ho ten cua ban"
+name = gets
+print "Nhap MSV cua ban"
+sid = gets
+print "Xin chao "+name + sid
+print "Nhap so bat ki tu ban phim"
+number = gets
+print "So vua nhap: "+number
+print "Nhap 2 so de so sanh"
+number1 = gets
+number2 = gets
+if number1 > number2
+  print "So lon la : "+number1
+elsif number1 < number2
+    puts "So lon la: "+ number2
+else
+    print "2 so bang nhau"+number1+number2
+end
